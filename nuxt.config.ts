df0e5ff682cf6c8287df0c5c28f665d2a1206306
huansis <98huan@gmail.com>
2026-04-24 17:07:28 +0800

    更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,13 +20,6 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       title: "Circuchem - Professional Copper Chemicals Supplier",
-      link: [
-        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
-        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
-        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
-        { rel: "manifest", href: "/site.webmanifest" },
-        { rel: "icon", href: "/favicon.ico" }
-      ]
     }
   },
   modules: ['@nuxt/content', '@nuxtjs/sitemap', '@nuxt/image', 'shadcn-nuxt', ],
